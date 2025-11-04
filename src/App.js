010e3673ec2ec00e183197bcc888ebb2411f015e
TheOriginalJiozx <omal0001@stud.ek.dk>
2025-11-04 11:15:53 +0100

    opdatering

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Register from "./pages/register";
 import Footer from "./components/footer";
 import Login from "./pages/login";
 import Me from "./pages/me";
+import ReadmePage from './pages/readme';
 
 function App() {
     useEffect(() => {
@@ -25,6 +26,7 @@ function App() {
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/me" element={<Me />} />
+                    <Route path="/readme" element={<ReadmePage />} />
                 </Routes>
             </main>
 
